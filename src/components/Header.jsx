6b1e4ad42b39027b84c726f933c71c8f180ea808
Heitor <heitor.moreira.neto@outlook.com>
2025-02-10 12:40:21 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Harry Potter</h1>
+            <p>O mundo mágico de Hogwarts</p>
+        </header>
+    );
+};
+
+export default Header;
